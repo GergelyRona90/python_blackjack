ebd5d3ee34024656f107f59cf5e7064f582bcc28
Gergely Róna <gergely.rona@budapestkozut.hu>
2023-10-17 14:58:35 +0200

    Create comments

--- main_blackjack.py
+++ main_blackjack.py
@@ -1,5 +1,5 @@
 import random
-
+# List of the cards
 cards_list = {"heart": [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10],
               "tiles": [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10],
               "pikes": [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10],
@@ -10,52 +10,65 @@ sum_own_cards = 0
 sum_opponent_cards = 0
 
 
+# def for deal
 def deal(sum_cards):
+    # Choose a random card type then a random card
     card_type = random.choice(list(cards_list.keys()))
     card = random.choice(cards_list[card_type])
+    # Remove the card from the deck
     cards_list[card_type].remove(card)
+    # Check the value of ace card
     if card == 11:
         if sum_cards + 11 > 21:
             card = 1
     return card_type, card
 
 
+# Printing text in different deal
+# Need the card type the card number to print these values
+# Need number_of_deal to know which turn is it
+# Sum_cards: This parameter contains the values of the cards
+    # if this equal to 0 then we don't count with this
+# blnOpponent_card is a boolean
+    # We check with this parameter whose turn it is
 def print_result(card_type, card_number, number_of_deal, sum_cards=0, blnOpponent_card=False):
-    import time  # Importáld a time modult
+    import time
 
-    list_of_deal = ["first", "second", "third", "forth", "fifth", "sixth", "seventh", "egihth", "ninth", "tenth",
+    list_of_deals = ["first", "second", "third", "forth", "fifth", "sixth", "seventh", "egihth", "ninth", "tenth",
                     "eleventh"]
     if sum_cards == 0:
         if not blnOpponent_card:
-            print(f"Your {list_of_deal[number_of_deal]} card: {card_type}, {card_number}\n")
+            print(f"Your {list_of_deals[number_of_deal]} card: {card_type}, {card_number}\n")
         else:
-            print(f"Your opponent's {list_of_deal[number_of_deal]} card: {card_type}, {card_number}\n")
+            print(f"Your opponent's {list_of_deals[number_of_deal]} card: {card_type}, {card_number}\n")
     else:
         if not blnOpponent_card:
-            print(f"Your {list_of_deal[number_of_deal]} card: {card_type}, {card_number}\nSum: {sum_cards}\n")
+            print(f"Your {list_of_deals[number_of_deal]} card: {card_type}, {card_number}\nSum: {sum_cards}\n")
         else:
             print(
-                f"Your opponent's {list_of_deal[number_of_deal]} card: {card_type}, {card_number}\nSum: {sum_cards}\n")
+                f"Your opponent's {list_of_deals[number_of_deal]} card: {card_type}, {card_number}\nSum: {sum_cards}\n")
 
-    time.sleep(1)  # Várakozás 1 másodpercig
+    time.sleep(1)  # Wait for 1 sec
 
 
 own_cards = []
 opponent_cards = []
 
-# Első deal magunknak
+# First deal for ourselves
 own_cards.append(deal(sum_own_cards))
 print_result(own_cards[0][0], own_cards[0][1], 0, 0, False)
-# Első deal ellenfélnek
+# First deal for opponent
 opponent_cards.append(deal(sum_opponent_cards))
 print_result(opponent_cards[0][0], opponent_cards[0][1], 0, 0, True)
-# Második deal magunknak
+# Second deal for ourselves
 own_cards.append(deal(sum_own_cards))
 sum_own_cards = own_cards[0][1] + own_cards[1][1]
 print_result(own_cards[1][0], own_cards[1][1], 1, sum_own_cards, False)
-# Második (rejtett) deal az elenfélnek
+# Second (hidden) deal for opponent
+    # Don't show opponent's second until we end the our turns
 opponent_cards.append(deal(sum_opponent_cards))
 sum_opponent_cards = opponent_cards[0][1] + opponent_cards[1][1]
+# Need a variable to store the turn of deals and use it for the print result function
 deal_turn = 2
 while not stop_game:
     answer = ""
